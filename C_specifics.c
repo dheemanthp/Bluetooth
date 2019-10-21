7c631f2cade12b5c05b477813fe506336ef36afe
dheemanthp <dvp499@gmail.com>
2019-10-20 21:13:36 -0700

    Update C_specifics.c

--- C_specifics.c
+++ C_specifics.c
@@ -1,4 +1,5 @@
 The memory region is divided into below regions
+https://www.geeksforgeeks.org/memory-layout-of-c-program/
 
 1)text or code segment
 there are executable instructions here
@@ -6,6 +7,18 @@ there are executable instructions here
 a) initialized data segment
   ->read/write only are static variables , global variables
   ->read only are const variables
-b) uninitialized data segment
-3)HEAP
-4)STACK
+b) uninitialized data segment [BSS - block started by symbol is named after an Assembly instruction]
+by default initialized to zero. by the kernel to arithmetic 0 before the program starts executing
+ contains all global variables and static variables that are initialized to zero 
+ or do not have explicit initialization in source code.
+For instance a variable declared static int i; would be contained in the BSS segment.
+For instance a global variable declared int j; would be contained in the BSS segment.   
+
+
+3)STACK
+Each time a function is called, the address of where to return to and certain information about the caller’s environment,
+such as some of the machine registers, are saved on the stack. The newly called function then allocates room on the stack for
+its automatic and temporary variables. This is how recursive functions in C can work. Each time a recursive function calls itself,
+a new stack frame is used, so one set of variables doesn’t interfere with the variables from another instance of the function.
+4)HEAP
+Heap area is managed by malloc, realloc, and free, which may use the brk and sbrk system calls to adjust its size 
