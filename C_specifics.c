a4adc6daf83ac179b4c56a282f5c8243b86654be
dheemanthp <dvp499@gmail.com>
2019-10-20 21:01:18 -0700

    Update C_specifics.c

--- C_specifics.c
+++ C_specifics.c
@@ -1,1 +1,11 @@
+The memory region is divided into below regions
 
+1)text or code segment
+there are executable instructions here
+2)data segment
+a) initialized data segment
+  ->read/write only are static variables , global variables
+  ->read only are const variables
+b) uninitialized data segment
+3)HEAP
+4)STACK
