baf41983eabcafe8b2e27f5010d6f1fb92e69a38
dheemanthp <dvp499@gmail.com>
2019-12-08 23:49:15 -0800

    Update BitWise.c

--- BitWise.c
+++ BitWise.c
@@ -6,6 +6,10 @@ https://www.geeksforgeeks.org/extract-k-bits-given-position-number/
 int bitExtracted(int number, int k, int p) 
 { 
     return (((1 << k) - 1) & (number >> (p - 1))); 
+    // ((1 << k) - 1) , will move 1 by 5 positions , which will become 32 , and subtracting by 1 , will give 31
+    // 0001 0000 --> 32
+    // 0000 1111 ---> 31
+    // (number >> (p - 1)) , will right shift the number by (p-1) positions
 } 
   
 // Driver code 
