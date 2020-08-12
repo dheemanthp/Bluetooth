8ce131edfcaa254897abe6ac1d52136e0f055922
dheemanthp <dvp499@gmail.com>
2020-08-12 02:43:47 -0700

    Update and rename reverse_string_recursively_normally.c to reverse_string_recursively_normally_palindrome.c

--- Apple/reverse_string_recursively_normally_palindrome.c
+++ Apple/reverse_string_recursively_normally_palindrome.c
@@ -38,14 +38,36 @@ void reverse_normally(char * name,int len,int index) {
 
 }
 
+bool checkPalidrome(char * str, int len) {
+    
+    int i = 0;
+    int j = len-1;
+    while(i < j) {
+        printf(" \n %c and %c ",str[i],str[j]);
+        if(str[i] != str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+
+
 int main()
 {
     char name[10] = {'d','h','e','e','m','a','n','t','h','\0'};
+    char str[10] = {'m','a','l','a','y','a','l','a','m','\0'};
+    
+    
     int len  = sizeof(name)/sizeof(name[0]);
     int index = -1;
     printf(" the len is %d",len);
     reverse_string_recursively(name, len-1,index);        
-    reverse_normally(name, len-2,index+1);        
+    reverse_normally(name, len-2,index+1);    
+    bool result = checkPalidrome(str,strlen(str));
+    printf(" \n palindrome result is %s\n",result?"true":"false");
     printf(" \nthe xyz string is %s\n",name);
     return 0;
 }
