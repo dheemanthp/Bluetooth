3201b6bab02736c05289a99118128178e21c69bb
dheemanthp <dvp499@gmail.com>
2020-08-13 02:51:24 -0700

    Update C_pointer_basics.c

--- Apple/C_pointer_basics.c
+++ Apple/C_pointer_basics.c
@@ -14,20 +14,40 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
+    //character array
     char arr1[10] = {'d','h','e','e','m','a','n','t','h','\0'};
     char arr2[50] = "dheemanth";//try to reduce and increase size here from 50 to say 3 , and see garbage output
     char *arr3 = "dheemanth";
     
+    //play with the 3 arrays
+    printf(" the strng1 is %s\n",arr1);
+    printf(" the len1 is %d\n",strlen(arr1));
+    printf(" the strng2 is %s\n",arr2);
+    printf(" the len2 is %d\n",strlen(arr2));
+    printf(" the strng3 is %s\n",arr3);
+    printf(" the len3 is %d\n",strlen(arr3));
+    for(int i = 0;i<50;i++){
+        printf(" char2 is %c\n",arr1[i]);
+        
+    }
+    printf(" the address of arr3 is %p\n",arr3);
+    printf(" the char  of arr3 is %c\n",*arr3);
+    printf(" the char  of arr3 is %c\n",*(arr3+1));//increasing the character address by 1 location
+    printf(" the char  of arr3 is %c\n",*arr3+1);//dereferencing and then doing a +1 to its achii value
+    printf(" the address of arr3 is %p\n",&arr3);
     
+    printf(" the address of arr3[0] is %p\n",arr3+0);
+    printf(" the address of arr3[1] is %p\n",arr3+1);
+    printf(" the address of arr3[1] is %p\n",arr3+2);
+    
+    
+    //integer pointers
     int *ptr = 21;
     printf(" the ptr address is %p\n",&ptr);
     printf(" the ptr contains is %d\n",ptr);
     //printf(" the ptr value is %d\n",*ptr); will cause SEG fault as you are assinging 21 to ptr instead of an address
     
-    
-    
-    //printf(" the ptr value is %d\n",*ptr);
-    
+    //integer pointers
     int num = 21;
     int * ptr1;
     ptr1 = &num;
@@ -36,36 +56,14 @@ int main()
     printf(" the ptr1 value is %d\n",*ptr1);
     
     
+    //2D arrays
+    int abc[2][3] = {};//init to all zeros
+    printf(" the abc value is %d\n",abc[0][0]);
+    printf(" the abc value is %d\n",abc[0][1]);
     
-    
-    
-    
-    
-    
-    
-    
-    
-    printf(" the strng1 is %s\n",arr1);
-    printf(" the len1 is %d\n",strlen(arr1));
-    printf(" the strng2 is %s\n",arr2);
-    printf(" the len2 is %d\n",strlen(arr2));
-    printf(" the strng3 is %s\n",arr3);
-    printf(" the len3 is %d\n",strlen(arr3));
-    
-    for(int i = 0;i<50;i++){
-        printf(" char2 is %c\n",arr1[i]);
-        
-    }
-    
-    printf(" the address of arr3 is %p\n",arr3);
-    printf(" the char  of arr3 is %c\n",*arr3);
-    printf(" the char  of arr3 is %c\n",*(arr3+1));
-    printf(" the char  of arr3 is %c\n",*arr3+1);
-    printf(" the address of arr3 is %p\n",&arr3);
-    
-    printf(" the address of arr3[0] is %p\n",arr3+0);
-    printf(" the address of arr3[1] is %p\n",arr3+1);
-    printf(" the address of arr3[1] is %p\n",arr3+2);
+    int xyz[2][3];//not initialized , so will contain all garbage
+    printf(" the xyz value is %d\n",xyz[0][0]);
+    printf(" the xyz value is %d\n",xyz[1][2]);
     
     
     
